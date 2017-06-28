2286f92b4a30f1bfafc89ec41d47b35f6beb8f87
Eric Hosford <eric.hosford@gmail.com>
2017-06-27 20:15:23 -0400

    setup basic server skeleton and project structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 8080;
+
+//dummy route
+server.get('/', (req, res) => {
+  res.send('it works');
+});
+
+server.listen(port, () => {
+  console.log('Now listening on port:', port);
+});
