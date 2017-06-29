034db0ab5670ab02f25f5febe1568baa68736530
Eric Hosford <eric.hosford@gmail.com>
2017-06-28 20:18:14 -0400

    built the router module that will handle weather requests and wired it up to our main application.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 8080;
 
+
+//routers
+const weatherRouter = require('./routers/weather.router');
+server.use(weatherRouter);
 //dummy route
 server.get('/', (req, res) => {
   res.send('it works');
