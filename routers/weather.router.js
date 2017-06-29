4ee4a0670da282e756bcd27c8e06ff24f8486eab
Eric Hosford <eric.hosford@gmail.com>
2017-06-29 18:47:52 -0400

    built weather route that allows consumers of our api to send lat and lon as part of the url.

--- routers/weather.router.js
+++ routers/weather.router.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const darksky = require('../credentials').darksky;
+const baseUrl = `https://api.darksky.net/forecast/${darksky}/`;
+
 
 router.get('/weather', (request, response) => {
-  response.send('here be the weather');
+  //console.log(request);
+  const url = `${baseUrl}37.8267,-122.4233`;
+  axios.get(url)
+    .then(weather => {
+      response.json(weather.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 router.get('/weather/:lat,:lon', (request, response) => {
-  response.send('here be the weather for that place');
+  const lat = request.params.lat;
+  const lon = request.params.lon;
+  const url = `${baseUrl}${lat},${lon}`;
+
+  axios.get(url)
+    .then(weather => {
+      response.json(weather.data);
+    })
+    .catch(err => {
+      console.error(err.stack);
+      response.status(404).send('Can\'t find that place');
+  });
 });
 module.exports = router;
