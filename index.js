d51668a0498ef92683830732d7e06817d4487c40
Eric Hosford <eric.hosford@gmail.com>
2017-07-12 19:44:25 -0400

    added cors package to allow cross origin request from client

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const port = process.env.PORT || 8080;
 const logger = require('./middleware/logger');
 const notFound = require('./middleware/404');
 const errorHandler = require('./middleware/500');
+const cors = require('cors')
 
 //routers
 const weatherRouter = require('./routers/weather.router');
 
 server.use(logger);
 server.use(weatherRouter);
+server.use(cors);
+
 //dummy route
 server.get('/', (req, res) => {
   res.send('it works');
